e7c3e1617c739d964c010665946243ca72cdb833
AlejandroRodriguezCollado <alex2005rodricoll@gmail.com>
2025-11-21 23:25:32 +0100

    unificar formatos de fecha

--- exploracion.py
+++ exploracion.py
@@ -3,15 +3,26 @@ import re
 import numpy as np
 
 def clean_date(txt):
-    res = re.search("(\d+)(\D+)(\d+)",txt)
-    res = re.search("(\d+)(\D+)(\d+)",txt)
+    res = re.search("(\d+)(\D+)(\d+)",txt) # primer mirem que no sigui un interval d'anys
     if res:
-        #res.group(2) != "-" and (abs((int(res.group(1)) - int(res.group(3))) / int(res.group(1))) <  0.1):
-        return True
-    return False
+        n1 = int(res.group(1))
+        n2 = int(res.group(3))
+        if (abs(n1 - n2) / n1) < 0.1:
+            return (n1 + n2)/2
+        else:
+            n1 = str(n1)
+            n2 = str(n2)
+            if len(n2) != 2:
+                print(txt)
+            return int(n1[:-2] + n2)
+    res = re.search("(\d+)", txt)
+    if res:
+        return int(res.group())
+    return np.nan
 
 columnas = ["ID", "FILE", "DATE","FORM", "TIMELINE"]
 df = pd.read_csv("labels.csv", sep="\t", usecols=columnas)
 df = df.loc[df["FORM"] == "painting"].copy()
-print(df.loc[df["DATE"].apply(clean_date)])
+df["CLEAN_DATE"] = df["DATE"].apply(clean_date)
+print(df.loc[(df["CLEAN_DATE"].isna())])
 
